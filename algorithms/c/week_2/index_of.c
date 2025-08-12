df47ab2d03a0d96e3fada8074c7245de5e294c58
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-08-11 23:56:37 -0300

    feat(algorithms/c): implement index_of algorithm
    Implements a function `index_of` that searches for an element `x` in an
    integer array `arr` of size `n`.
    Returns the index of the first occurrence of `x` if found, otherwise
    returns -1.
    Includes `main` function with test cases to demonstrate usage and
    verify correctness.
    
    Adds a common array searching algorithm to the collection. Provides a basic example of C programming for algorithm implementation.

---
+++ algorithms/c/week_2/index_of.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int index_of(const int *arr, size_t n, int x);
+
+int main() {
+	int arr_1[3] = {3, 5, 7};
+	int arr_2[3] = {1, 1, 1};
+	int *arr_3 = NULL;
+
+	printf("First array [3, 5, 7] -> x = 5: %d (SHOULD BE 1)\n", index_of(arr_1, sizeof(arr_1) / sizeof(arr_1[0]), 5));
+	printf("Second array [1, 1, 1] -> x = 2: %d (SHOULD BE -1)\n", index_of(arr_2, sizeof(arr_2) / sizeof(arr_2[0]), 2));
+	printf("Third array [] -> x = 9: %d (SHOULD BE -1)", index_of(arr_3, 0, 9));
+}
+
+int index_of(const int *arr, size_t n, int x) {
+	int index = -1;
+	for (size_t i = 0; i < n; i++) { 
+		if (*(arr + i) == x) {
+			index = i; 
+			break; 
+		}
+	}
+	return index;
+}
+
